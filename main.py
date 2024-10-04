5e73de96a06c53b3b033a5b4f4c62e24441dda23
Likith-23 <likithsairajpignos@gmail.com>
2024-10-04 21:12:38 +0200

    intial

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
-larger_number = int(input("ENTER A LARGE NUMBER"))
-smaller_number = int(input("ENTER A SMALL NUMBER"))
-while (smaller_number):
-    number_store = smaller_number
-    smaller_number = larger_number % smaller_number
-    larger_number = number_store
-print("HCF is...", larger_number)
-    
+larger_number = int(input("ENTER A LARGE NUMBER: "))
+smaller_number = int(input("ENTER A SMALL NUMBER: "))
+def calculate_lcm(larger_number, smaller_number):
+    original_larger = larger_number
+    original_smaller = smaller_number
+    while smaller_number:
+        number_store = smaller_number
+        smaller_number = larger_number % smaller_number
+        larger_number = number_store
+    lcm = abs(original_larger * original_smaller) // number_store
+    return lcm
+lcm_result = calculate_lcm(larger_number, smaller_number)
+print("THE LCM IS >>>", lcm_result)
